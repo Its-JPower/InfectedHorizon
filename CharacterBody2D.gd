6eff84671c2b7893ad7c2d4e77b42434c27e6fe3
Its-JPower <joseva15@kingshigh.school.nz>
2024-06-26 11:55:43 +1200

    Movement finished (clciktomove+keyboard)

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -12,18 +12,19 @@ var target = Vector2.ZERO
 
 func _physics_process(delta):
 	direction = Input.get_vector("left","right","up","down").normalized()
-	sprite.rotation = global_position.direction_to(get_global_mouse_position()).angle()
 	if Input.is_action_pressed("move"):
 		target = get_global_mouse_position()
 	if global_position.distance_to(target) > 5:
 		if Input.is_action_pressed("move"):
 			velocity = global_position.direction_to(target) * speed
+			sprite.rotation = velocity.angle()
 			if sprite.animation == "default":
 				sprite.play("default")
 	if velocity.x == 0:
 		sprite.play("default")
 	if direction:
 		velocity = velocity.move_toward(direction * speed, acceleration)
+		sprite.rotation = velocity.angle()
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, friction)
 	move_and_slide()
