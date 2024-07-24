96f2051e6929d0328aaa40ef612e20060163abfb
Its-JPower <joseva15@kingshigh.school.nz>
2024-07-25 09:54:06 +1200

    anim provlems trying to fix

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -5,11 +5,11 @@ extends CharacterBody2D
 @export var acceleration = 40.0
 @onready var sprite = $Sprite
 @export var friction = 50.0
-@export var shot = false
+@export var shoot = false
 @export var reload = false
+@export var moving = false
 
-@onready var animator = $AnimatedSprite2D
-@onready var animation_player = $AnimationPlayer
+@onready var rich_text_label = $Camera2D/RichTextLabel
 
 var direction = Vector2.ZERO
 var target = Vector2.ZERO
@@ -19,9 +19,35 @@ func _physics_process(delta):
 	if direction:
 		velocity = velocity.move_toward(direction * speed, acceleration)
 		sprite.rotation = velocity.angle()
+		moving = true
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, friction)
+		moving = false
 	if Input.is_action_just_pressed("reload"):
-		reload = true
-		sprite.play("handgun_reload")
+		if reload == true:
+			pass
+		else:
+			reload = true
+			sprite.play("handgun_reload")
+			sprite.frame = 0
 	move_and_slide()
+	rich_text_label.text = "Animation: "+sprite.animation+" Animation Frame: "+str(sprite.frame)+"  Reload: "+str(reload)
+
+
+func _on_sprite_animation_finished():
+	if sprite.animation == "handgun_reload":
+		reload = false
+		if velocity != Vector2.ZERO:
+			sprite.play("handgun_move")
+			sprite.frame = 0
+		else:
+			sprite.play("handgun_idle")
+	elif sprite.animation == "handgun_move" and velocity == Vector2.ZERO:
+		sprite.play("handgun_idle")
+	elif sprite.animation != "handgun_reload":
+		sprite.play("handgun_move")
+		sprite.frame = 0
+
+
+func _on_sprite_animation_changed():
+	sprite.frame = 0
