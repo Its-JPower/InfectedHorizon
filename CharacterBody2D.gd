ce61fa239355cc234fe55c6494d10651b7bb16a1
Its-JPower <joseva15@kingshigh.school.nz>
2024-08-02 14:23:14 +1200

    smooth rotation added

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,22 +1,30 @@
 extends CharacterBody2D
 
 
-@export var speed = 200.0
-@export var acceleration = 40.0
+@export var speed : float = 100.0 #normal speed
+@export var sprint : float = 150.0 #speed when sprinting
+@export var stamina : float = 100.0 #stamina for sprinting
+@export var acceleration : float = 40.0
 @onready var player = $Sprite
 @onready var anim_player = $AnimationPlayer
 @export var friction = 50.0
-var shooting = false
-var reloading = false
+@export var rotation_speed: float = 5.0  #rotation speed for player rotation
 
+var scoped : bool = false
+var shooting : bool = false
+var reloading : bool = false
+var sprinting : bool = false
 var direction = Vector2.ZERO
 var target = Vector2.ZERO
+var target_rotation = 0.0
+
 
 func _physics_process(delta):
 	direction = Input.get_vector("left","right","up","down").normalized()                                                            
 	if direction:
 		velocity = velocity.move_toward(direction * speed, acceleration)
-		player.rotation = velocity.angle()
+		target_rotation = direction.angle()
+		player.rotation = lerp_angle(player.rotation, target_rotation, rotation_speed * delta)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, friction)
 	if Input.is_action_just_pressed("reload"):
@@ -27,4 +35,7 @@ func _physics_process(delta):
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "handgun_reload" or anim_name == "handgun_shoot":
-		anim_player.play("handgun_idle")
+		if sprinting == false:
+			anim_player.play("handgun_idle")
+		else:
+			anim_player.play("handgun_move")
