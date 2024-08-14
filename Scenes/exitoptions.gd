e0437b2948a7048aa37f3afefc6664647f236e63
Its-JPower <joseva15@kingshigh.school.nz>
2024-08-14 12:25:04 +1200

    options progress
    added to player scene

--- Scenes/exitoptions.gd
+++ Scenes/exitoptions.gd
@@ -3,7 +3,12 @@ extends Button
 
 @onready var menu = $"../../Menu"
 @onready var options = $".."
+@onready var option = $"../../Control"
 
 func _pressed():
-	menu.visible = true
-	options.visible = false
+	if get_tree().current_scene == "res://Scenes/start_menu.tscn":
+		options.visible = false
+		option.visible = true
+	else:
+		options.visible = false
+		menu.visible = true
--- Scenes/mainmusic_slider.gd
+++ Scenes/mainmusic_slider.gd
@@ -2,10 +2,13 @@ extends HSlider
 
 
 @onready var menu_music = $"../../../../../Menu Music"
+@onready var bullet_audio = $Audio
 
 func _value_changed(new_value):
 	if new_value == -100:
 		menu_music.volume_db = -80
+		bullet_audio.volume_db = -80
 	else:
 		menu_music.volume_db = new_value*(0.15)
+		bullet_audio.volume_db = new_value*(0.15)
 	print(menu_music.volume_db)
