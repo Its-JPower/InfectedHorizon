b49231c5f9fa4636dddef453fe3f5a9515713d4c
Its-JPower <joseva15@kingshigh.school.nz>
2024-09-20 10:41:56 +1200

    File Organization

--- Scripts/info.gd
+++ Scripts/info.gd
@@ -5,7 +5,7 @@ extends Label
 func _ready() -> void:
 	text = "ⓘ W-A-S-D to Move
 	ⓘ Left Click to Shoot
-	ⓘ Right Click to Scope-in
+	ⓘ Right Click or Q to Scope-in
 	ⓘ 1-2 to Cycle Weapons
 	ⓘ R to reload"
 	await get_tree().create_timer(10.0).timeout
